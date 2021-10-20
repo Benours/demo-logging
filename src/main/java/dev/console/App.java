77700d2062d47218aa2a8f4ec77a70c8d71afef0
benours <benjamin.baska@gmail.com>
2021-10-20 14:53:26 +0200

    fin tp

---
+++ src/main/java/dev/console/App.java
@@ -0,0 +1,22 @@
+package dev.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.service.AppService;
+
+
+public class App {
+
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implementation Logback");
+		AppService app = new AppService();
+		app.executer("Hello");
+
+	}
+
+}
